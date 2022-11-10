0bd58bd442ccb806eb6d70cbdd1077bf555606db
DeviousWings <nksteele@byui.edu>
2022-11-10 17:46:39 -0600

    first commit

---
+++ hilo/game/hilo.py
@@ -0,0 +1,48 @@
+import random
+"""The card is: 9
+Higher or Lower? [h/l] l
+Next card was: 5
+Your score is: 400
+Play again? [y/n]
+
+"""
+class Hilo:
+    
+    def __init__(self):
+        self.cards = []
+        
+        
+        
+    def draw(self):
+        value = 1
+        for card in self.cards:
+            self.cards.append([value])
+            value = value + 1
+        
+        random.shuffle(self.cards)
+        score = 300
+        card1 = self.cards
+        
+        value = random.shuffle(self.cards)
+        self.points = print(f"Your score is: {score}")
+        print(f"The current card is {card1 [0]}")
+
+        card2 = self.cards
+        print(f"Your score is: {score}")
+        
+        self.choice = input("higher or lower? ")
+        
+        card2 = self.cards
+        print(f"The next card is {card2[0]}")
+        
+        if self.choice == "h" and card2[1] > card1[1]:
+            score += 100
+        elif self.choice =="l" and card2[1] < card1[1]:
+            score += 100
+        elif self.choice =="h" and card2[1] == card1[1]:
+            self.score += 0
+        elif self.choice =="l" and card2[1] == card1[1]:
+            score += 0
+        else:
+            score -= 75
+        card1 = card2
